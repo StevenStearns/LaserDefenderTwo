67efb64f33e3362570e6b1814fdf29061b2f0f33
steven stearns <stearn102@morrisville.edu>
2022-04-26 15:08:20 -0400

    boundaries.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,16 +7,38 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float fltMoveSpeed = 5f;
     Vector2 rawInput;
-  
+
+    [SerializeField] float fltPaddingLeft;
+    [SerializeField] float fltPaddingRight;
+    [SerializeField] float fltPaddingTop;
+    [SerializeField] float fltPaddingBottom;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     void Move()
     {
-        Vector3 delta = rawInput * fltMoveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * fltMoveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + fltPaddingLeft, maxBounds.x - fltPaddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + fltPaddingBottom, maxBounds.y - fltPaddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
