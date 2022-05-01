fffb482d425f7eba88fd01623b240828076fd6a3
steven stearns <stearn102@morrisville.edu>
2022-04-30 23:06:41 -0400

    Enemy Shooting

--- Assets/Script/Shooter.cs
+++ Assets/Script/Shooter.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float fltProjectileSpeed = 10f;
     [SerializeField] float fltProjectileLifetime = 5f;
-    [SerializeField] float fltFiringRate = 0.2f; 
+    [SerializeField] float fltBaseFiringRate = 0.2f;
 
-    public bool blIsFiring;
+    [Header("AI")]
+    [SerializeField] bool blUseAI;
+    [SerializeField] float fltFiringRateVariance = 0f;
+    [SerializeField] float fltMinimumFiringRate = 0.1f;
+
+    [HideInInspector] public bool blIsFiring;
 
     Coroutine firingCoroutine;
 
     void Start()
     {
+        if(blUseAI)
+        {
+            blIsFiring = true;
+        }
         
     }
 
@@ -51,7 +61,10 @@ public class Shooter : MonoBehaviour
             }
 
             Destroy(instance, fltProjectileLifetime);
-            yield return new WaitForSeconds(fltFiringRate);
+            float fltTimeToNextProjectile = Random.Range(fltBaseFiringRate - fltFiringRateVariance,
+                                                        fltBaseFiringRate + fltFiringRateVariance);
+            fltTimeToNextProjectile = Mathf.Clamp(fltTimeToNextProjectile, fltMinimumFiringRate, float.MaxValue);
+            yield return new WaitForSeconds(fltTimeToNextProjectile);
         }
     }
 }
