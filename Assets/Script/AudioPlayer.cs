9d6b341edfbe45ae312870a71e472430ebff0066
steven stearns <stearn102@morrisville.edu>
2022-05-08 00:36:17 -0400

    Singletons

--- Assets/Script/AudioPlayer.cs
+++ Assets/Script/AudioPlayer.cs
@@ -12,6 +12,29 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] AudioClip damageClip;
     [SerializeField] [Range(0f, 1f)] float fltDamageVolume = 1f;
 
+    static AudioPlayer instance;
+
+    
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public void PlayShootingClip()
     {
         PlayClip(shootingClip, fltShootingVolume);
